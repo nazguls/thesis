ac179dc6286cf8431d68c253ddbe983efbf73cfb
Adam Watt <awattny@me.com>
2017-02-22 13:41:22 -0800

    fix code indentation

--- client/src/components/home/drawerComponent.js
+++ client/src/components/home/drawerComponent.js
@@ -4,27 +4,27 @@ import UserProfile from './userProfile';
 import {Actions, DefaultRenderer} from 'react-native-router-flux';
 
 export default class NavigationDrawer extends React.Component {
-    render(){
-        const state = this.props.navigationState;
-        const children = state.children;
+  render(){
+    const state = this.props.navigationState;
+    const children = state.children;
 
-        return (
-            <Drawer
-                ref="navigation"
-                open={state.open}
-                onOpen={()=>Actions.refresh({key:state.key, open: true})}
-                onClose={()=>Actions.refresh({key:state.key, open: false})}
-                type="displace"
-                content={<UserProfile  />}
-                tapToClose={true}
-                openDrawerOffset={0.3}
-                panCloseMask={0.2}
-                negotiatePan={true}
-                tweenHandler={(ratio) => ({
-                 main: { opacity:Math.max(0.54,1-ratio) }
-            })}>
-                <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
-            </Drawer>
-        );
-    }
+    return (
+      <Drawer
+        ref="navigation"
+        open={state.open}
+        onOpen={()=>Actions.refresh({key:state.key, open: true})}
+        onClose={()=>Actions.refresh({key:state.key, open: false})}
+        type="displace"
+        content={<UserProfile  />}
+        tapToClose={true}
+        openDrawerOffset={0.3}
+        panCloseMask={0.2}
+        negotiatePan={true}
+        tweenHandler={(ratio) => ({
+         main: { opacity:Math.max(0.54,1-ratio) }
+      })}>
+        <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
+      </Drawer>
+    );
+  }
 }
