925d71aa6b80a4977b5948d75d04d9a2d2090783
Adam Watt <awattny@me.com>
2017-02-22 11:29:02 -0800

    add drawer scene to router + fix padding issue + clean up code

--- client/src/Router.js
+++ client/src/Router.js
@@ -5,95 +5,48 @@ import Home from './components/home/index';
 import IndStock from './components/indStock/index';
 import buy from './components/indStock/buy';
 import sell from './components/indStock/sell';
-import userProfile from './components/home/slideUserProfile'
+import drawer from './components/home/drawerComponent';
 import search from './components/home/search';
-
-////////////////////////////////////////////////////////////////////////
-//icons//
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import Search from 'react-native-vector-icons/Foundation';
-////////////////////////////////////////////////////////////////////////
+import accountIcon from './components/assets/ic_account_circle.png';
 
 const RouterComponent = () => {
 	return (
-		<Router sceneStyle={{ paddingTop: 65 }}>
+		<Router >
 			<Scene key="auth">
 				<Scene key='login' component={LoginForm} title="Please Login" />
 			</Scene>
-
-			<Scene key="main" initial >
-				<Scene
+      <Scene 
+        key="drawer" 
+        component={drawer} 
+        open={false}
+        initial 
+      >
+			<Scene key="main" >
+        <Scene
+          sceneStyle={{ paddingTop: 60 }}
 					key='home'
 					component={Home}
 					title="DashBoard"
 					rightTitle="search"
-
+          leftButtonImage={accountIcon}
 					onRight={() => Actions.search()}
+          onLeft={() => Actions.refresh({ key: 'drawer', open: value => !value })}
+        />
+        <Scene 
+          key='indStock' 
+          component={IndStock} 
+          title="Stock Description" 
+          sceneStyle={{ paddingTop: 60 }} 
+        />
+				<Scene key='buy' component={buy} title="Market Buy" sceneStyle={{ paddingTop: 60 }} />
+				<Scene key='sell' component={sell} title="Market Sell" sceneStyle={{ paddingTop: 60 }} />
+        
 
-					leftTitle="profile"
-					onLeft={() => Actions.userProfile()}
-				/>
-				<Scene key='indStock' component={IndStock} title="Stock Description" />
-				<Scene key='buy' component={buy} title="Market Buy" />
-				<Scene key='sell' component={sell} title="Market Sell" />
-        <Scene key='userProfile' component={userProfile} title="User Profile" />
-
-        <Scene key='search' component={search} title="Search" />
+        <Scene key='search' component={search} title="Search" sceneStyle={{ paddingTop: 60 }} />
 			</Scene>
-
-
-		</Router>
+      </Scene>
+    </Router>
 	);
 };
 
 export default RouterComponent;
-
-/**
-import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View } from 'react-native';
-//handles routing
-// import { Router, Scene } from 'react-native-router-flux';
-
-
-//keep track of all the pages
-import Index from './src/components/home/index';
-import IndStock from './src/components/indStock/index';
-import Buy from './src/components/indStock/buy'
-import BuySell from './src/components/indStock/buySell'
-
-
-class App extends Component {
-
-  //function that renderscene is using
-  constructor() {
-    super()
-    this.renderScene= this.renderScene.bind(this);
-  }
-
-  renderScene(route, navigator) {
-    // _navigator = navigator;
-    switch (route.id) {
-      case 'index':
-        return (<Index navigator={navigator} title = "index" />);
-      case 'indStock':
-        return (<IndStock navigator={navigator} title = 'indStock' />);
-      case 'buy':
-        return (<Buy navigator={navigator} title = 'buy' />)
-    }
-    // if(route.id === 'index'){
-    //   return <Index navigator={navigator} />;
-    // }
-  }
-
-  render() {
-
-    return (
-      <Navigator
-        initialRoute={{id: 'index'}}
-        renderScene={ this.renderScene }
-      />
-    );
-  }
-}
-
-**/
