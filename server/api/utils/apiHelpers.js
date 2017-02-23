07f3e06d756b4b391ccadfc0578c947c547d7c2f
Michael <comesm@gmail.com>
2017-02-23 12:15:12 -0800

    refactor date formatting into separate functions

--- server/api/utils/apiHelpers.js
+++ server/api/utils/apiHelpers.js
@@ -12,66 +12,62 @@ const getStockPrice = (ticker) =>
  const getHistoricalPrices = (ticker, options) => {
 
 let currentDate = new Date();
-let startDate = new Date();
+//let startDate = new Date();
 let numPeriods = options.numperiods;
-if(options.type === 'day') {
-  startDate.setDate(startDate.getDate() - numPeriods);
-}
-else if(options.type === 'month') {
-  startDate.setMonth(startDate.getMonth() - numPeriods);
-}
-else if(options.type === 'year') {
-  startDate.setFullYear(startDate.getFullYear() - numPeriods);
-}
-else if(options.type === 'week') {
-  startDate.setDate(startDate.getDate() - (numPeriods * 7));
-}
+let period = options.type;
+// if(options.type === 'day') {
+//   startDate.setDate(startDate.getDate() - numPeriods);
+// }
+// else if(options.type === 'month') {
+//   startDate.setMonth(startDate.getMonth() - numPeriods);
+// }
+// else if(options.type === 'year') {
+//   startDate.setFullYear(startDate.getFullYear() - numPeriods);
+// }
+// else if(options.type === 'week') {
+//   startDate.setDate(startDate.getDate() - (numPeriods * 7));
+// }
 
-let startMonth = startDate.getMonth()+1;
-let endMonth = currentDate.getMonth()+1;
+let startDate = calculateStart(options.type, numPeriods)
 
-let startMonthFormatted = startMonth + 1 > 9 ? startMonth :
-  '0' + startMonth;
+let formattedStart = formatDate(startDate);
+let formattedCurrentDate = formatDate(currentDate);
 
-  let startDayFormatted = startDate.getDate() > 9 ? startDate.getDate() :
-  '0' + startDate.getDate();
-
-let formattedStart = startDate.getFullYear()+'-'+startMonthFormatted+'-'+startDayFormatted+'T00:00:00-00';
-
-
-let currentMonth = endMonth + 1 > 9 ? endMonth :
-  '0' + endMonth;
-
-let formatCurrentDay = currentDate.getDate() > 9 ? currentDate.getDate() : '0' + currentDate.getDate();
-
-let formattedCurrent = currentDate.getFullYear()+'-'+currentMonth+'-'+formatCurrentDay+'T00:00:00-00';
-//2011-03-01T00:00:00-00
-console.log('start: ', formattedStart);
-console.log('end: ', formattedCurrent);
-console.log('type: ', options.type);
-
-// > a.getFullYear()
-// 2017
- // startDate.setFullYear(startDate.getFullYear() - 2)
-
-// var d = new Date();
-// d.setDate(d.getDate() - 2);
-// > a.getMonth()
-// 1
-// > a.setMonth(a.getMonth() - 2)
-// 1482455475157
-
- //{ period: 'historical', numperiods: '55', type: 'day' }
   const inputOptions = { params:
   { parameters: { "Normalized": false, "StartDate":
-    formattedStart , "EndDate": formattedCurrent,
-    "DataPeriod": options.type, "Elements":[ {"Symbol": ticker, "Type": "price", "Params":["c"]}]}}}
+    formattedStart , "EndDate": formattedCurrentDate,
+    "DataPeriod": period, "Elements":[ {"Symbol": ticker, "Type": "price", "Params":["c"]}]}}}
 
   return axios.get('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json', inputOptions)
   .catch(err => console.log('20', err));
 };
 
-// http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"AAPL","Type":"price","Params":["c"]}]}
+ const formatDate = (date) => {
+
+   const month = date.getMonth() + 1;
+   const monthFormatted = month + 1 > 9 ? month :
+     '0' + month;
+   const dayFormatted = date.getDate() > 9 ? date.getDate() :
+     '0' + date.getDate();
+
+   const formattedDate = date.getFullYear()+'-'+monthFormatted+'-'+dayFormatted+'T00:00:00-00';
+   return formattedDate;
+ };
+
+ const calculateStart = (period, numPeriods) => {
+    const start = new Date();
+
+  if (period === 'day') {
+    start.setDate(start.getDate() - numPeriods);
+  } else if (period === 'month') {
+    start.setMonth(start.getMonth() - numPeriods);
+  } else if (period === 'year') {
+    start.setFullYear(start.getFullYear() - numPeriods);
+  } else if (period === 'week') {
+    start.setDate(start.getDate() - (numPeriods * 7));
+  }
+  return start;
+ };
 
 
  const getBulkStockPrices = (portfolio) => {
