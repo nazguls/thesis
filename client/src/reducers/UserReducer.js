895c2f58ae0a1ad3c6059afd51a39be2b6b892b5
Adam Watt <awattny@me.com>
2017-02-23 16:32:37 -0800

    create Userreducer with initial state

---
+++ client/src/reducers/UserReducer.js
@@ -0,0 +1,16 @@
+import {
+	
+} from '../actions/types';
+
+const INITIAL_STATE = {
+	name: 'Isaac Yoon',
+	mktValue: 0,
+	cash: 0
+};
+
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
