32a36a556fb5993e8226a36c8c248f0eeb70cbe4
Adam Watt <awattny@me.com>
2017-02-28 15:30:32 -0800

    add cash column to portfolio model table

--- db/portfolioModel.js
+++ db/portfolioModel.js
@@ -4,6 +4,7 @@ const connection = require('./dbConnect');
 const Portfolio = connection.define('Portfolios', {
   date: Sequelize.DATE,
   portfolioValue: Sequelize.FLOAT,
+  cash: Sequelize.FLOAT
 });
 
 module.exports = Portfolio;
