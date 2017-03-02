29c53ed679dcc5fb3305e37e7f2c21a44cab9aeb
Michael <comesm@gmail.com>
2017-03-01 20:30:11 -0800

    add S&P data chart

--- client/src/components/home/chart.js
+++ client/src/components/home/chart.js
@@ -16,7 +16,7 @@ const dimensionsWindow = Dimensions.get('window');
 
 const xAccessor = (d) => { return d.date; };
 
-const yAccessor = (d) => { return d.value; };
+const yAccessor = (d) => { return d.value !== "." ? d.value; };
 
 class Chart extends Component {
 
