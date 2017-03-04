a6a502def7f8059098c240c71339116fc5c46446
Adam Watt <awattny@me.com>
2017-03-03 17:33:41 -0800

    add firstnamechanged lastnamechanged and usernamechanged types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
+export const FIRST_NAME_CHANGED = 'first_name_changed';
+export const LAST_NAME_CHANGED = 'last_name_changed';
+export const USERNAME_CHANGED = 'username_changed';
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
