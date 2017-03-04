38537ba7adcd0c7f208d7cf0ebfc9afc18214b36
Adam Watt <awattny@me.com>
2017-03-03 17:38:49 -0800

    code clean up

--- server/api/money/moneyController.js
+++ server/api/money/moneyController.js
@@ -1,14 +1,14 @@
 const dbHelper = require('../utils/dbHelpers');
 
 exports.post = (req, res) => {
-  let user = req.params.user;
-  let depositData = req.body;
+  const user = req.params.user;
+  const depositData = req.body;
   dbHelper.deposit(depositData, user).then(data =>
   res.send(data));
 };
 
 exports.get = (req, res) => {
   const user = req.params.user;
-  dbHelper.getCash(user).then(data =>
-  res.send(data));
+  dbHelper.getCash(user)
+  .then(data => res.send(data));
 };
