d7e1d38005a8db1dc33ea35ea0d0a8396608949d
Adam Watt <awattny@me.com>
2017-03-03 17:34:37 -0800

    update user reducer to handle signup component

--- client/src/reducers/UserReducer.js
+++ client/src/reducers/UserReducer.js
@@ -1,16 +1,27 @@
 import {
+	USERNAME_CHANGED,
+  FIRST_NAME_CHANGED,
+  LAST_NAME_CHANGED,	
   MARKET_VALUE,
   CASH_VALUE
 } from '../actions/types';
 
 const INITIAL_STATE = {
-	name: 'Isaac Yoon',
+	username: '',
+	firstName: '',
+	lastName: '',
 	mktValue: 0,
 	cashValue: 0
 };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case USERNAME_CHANGED:
+			return { ...state, username: action.payload };
+		case FIRST_NAME_CHANGED:
+			return { ...state, firstName: action.payload };
+		case LAST_NAME_CHANGED:
+			return { ...state, lastName: action.payload };
 		case MARKET_VALUE:
 			return { ...state, mktValue: action.payload };
 		case CASH_VALUE:
