42d67458166b7d4e06feb8dd4eff16df84d2b4b1
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-04 17:07:42 -0800

    user can't sell more than their current holding

--- client/src/components/indStock/sell.js
+++ client/src/components/indStock/sell.js
@@ -1,3 +1,4 @@
+import { Text } from 'react-native';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
@@ -6,39 +7,75 @@ import { Background, CardSection, Input, Button } from '../common/';
 import { updateStockShare, updateCashValue } from '../../actions';
 
 class Sell extends Component {
+	// constructor() {
+	// 	super();
 
+	// 	this.state = {
+	// 		currentShare: 0
+	// 	};
+	// }
 	onSharesChange(text) {
 		this.props.updateStockShare(text);
 	}
 
 	onButtonPress(symbol) {
-		const context = this;
-		axios({
-			method: 'post',
-			url: `http://127.0.0.1:3000/api/stocks/${symbol}`,
-			data: {
-				stock: context.props.stockRes.data.Symbol,
-				transact: 'sell',
-				userId: 1,
-				price: context.props.stockRes.data.LastPrice,
-				shares: context.props.stockShare,
+		if (this.props.stockShare > this.currentShares()) {
+			console.log('You dont have enough shares');
+		} else {
 
+			const context = this;
+			axios({
+				method: 'post',
+				url: `http://127.0.0.1:3000/api/stocks/${symbol}`,
+				data: {
+					stock: context.props.stockRes.data.Symbol,
+					transact: 'sell',
+					userId: 1,
+					price: context.props.stockRes.data.LastPrice,
+					shares: context.props.stockShare,
+
+				}
+			}).then(() => {
+				Actions.home({ type: 'reset' });
+				const price = context.props.stockRes.data.LastPrice;
+				const numShares = context.props.stockShare;
+				const preBuyCashValue = context.props.cashValue;
+				const newCashValue = preBuyCashValue + (price * numShares);
+				const roundedCashValue = Math.round(newCashValue * 100) / 100;
+				context.props.updateCashValue(roundedCashValue);
+			}).catch(error => console.log(error));
+		}
+	}
+
+	currentShares() {
+		for (let i = 0; i < this.props.numShares.length; i++) {
+			if (this.props.numShares[i].symbol === this.props.stockRes.data.Symbol) {
+				return JSON.stringify(this.props.numShares[i].numOfShares);
 			}
-		}).then(() => {
-			Actions.home({ type: 'reset' });
-			const price = context.props.stockRes.data.LastPrice;
-			const numShares = context.props.stockShare;
-			const preBuyCashValue = context.props.cashValue;
-			const newCashValue = preBuyCashValue + (price * numShares);
-			const roundedCashValue = Math.round(newCashValue * 100) / 100;
-			context.props.updateCashValue(roundedCashValue);
-		}).catch(error => console.log(error));
+		}
+	}
+	errorMessage() {
+		if (this.props.stockShare > this.currentShares()) {
+			return (
+				<Text style={{ color: 'red' }}> You are trying to sell more than your current holding </Text>
+			);
+		}
 	}
 
 	render() {
+		console.log('num of Shares', this.props.numShares);
+		console.log('stockRes', this.props.stockShare);
+
 		const { stockRes, stockShare } = this.props;
+		console.log('stockShare', this.props.stockShare);
 		return (
 			<Background>
+				<CardSection>
+					<Input
+						label="Current Shares"
+						placeholder={this.currentShares()}
+					/>
+				</CardSection>
 				<CardSection>
 					<Input
 						label="Shares"
@@ -58,7 +95,7 @@ class Sell extends Component {
 				<CardSection>
 					<Input
 						label="EST Cost"
-						placeholder={JSON.stringify(Math.round(stockRes.data.LastPrice * stockShare*100)/100)}
+						placeholder={JSON.stringify(Math.round(stockRes.data.LastPrice * stockShare * 100) / 100)}
 					/>
 				</CardSection>
 
@@ -67,7 +104,11 @@ class Sell extends Component {
 						Confirm
 					</Button>
 				</CardSection>
+				<CardSection>
+					{this.errorMessage()}
+				</CardSection>
 			</Background>
+
 		);
 	}
 }
@@ -75,11 +116,12 @@ class Sell extends Component {
 
 const mapStateToProps = (state) => {
 	const { stockRes, stockShare } = state.search;
-	const { cashValue } = state.user;
+	const { cashValue, numShares } = state.user;
 	return ({
 		stockShare,
 		stockRes,
-		cashValue
+		cashValue,
+		numShares
 	});
 };
 
