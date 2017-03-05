b20b60b544f4c26a0c85d3d5a81bff511aa5908f
Adam Watt <awattny@me.com>
2017-03-04 17:06:28 -0800

    clean code

--- client/src/components/home/drawerComponent.js
+++ client/src/components/home/drawerComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Drawer from 'react-native-drawer';
+import { Actions, DefaultRenderer } from 'react-native-router-flux';
 import UserProfile from './userProfile';
-import {Actions, DefaultRenderer} from 'react-native-router-flux';
+
 
 export default class NavigationDrawer extends React.Component {
-  render(){
+  render() {
     const state = this.props.navigationState;
     const children = state.children;
 
@@ -12,17 +13,18 @@ export default class NavigationDrawer extends React.Component {
       <Drawer
         ref="navigation"
         open={state.open}
-        onOpen={()=>Actions.refresh({key:state.key, open: true})}
-        onClose={()=>Actions.refresh({key:state.key, open: false})}
+        onOpen={() => Actions.refresh({ key: state.key, open: true })}
+        onClose={() => Actions.refresh({ key: state.key, open: false })}
         type="displace"
-        content={<UserProfile  />}
+        content={<UserProfile />}
         tapToClose={true}
         openDrawerOffset={0.3}
         panCloseMask={0.2}
         negotiatePan={true}
         tweenHandler={(ratio) => ({
-         main: { opacity:Math.max(0.54,1-ratio) }
-      })}>
+         main: { opacity: Math.max(0.54, 1 - ratio) }
+      })}
+      >
         <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
       </Drawer>
     );
