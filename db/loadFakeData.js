0cc187e8b054d2a07591c9c8a04b48ed15778294
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-07 18:20:33 -0800

    adjust fakedData

--- db/loadFakeData.js
+++ db/loadFakeData.js
@@ -5,10 +5,22 @@ const fakeUserData = require('./fakeData/fakeUserData.json');
 const fakeTransactions = require('./fakeData/fakeTransactions.json');
 const fakeUserTransactionData = require('./fakeData/fakeUserTransactionData.json');
 const fakeUserStocksData = require('./fakeData/fakeUserStocksData.json');
+const fakePortfolioData = require('./fakeData/fakePortfolioData.json');
+const fakeUserPortfolioData = require('./fakeData/fakeUserPortfolioData.json');
 
-console.log(fakeUserData);
+console.log(fakeStockData);
 
 Models.User.bulkCreate(fakeUserData, { individualHooks: true })
+.then(() => {
+  return Models.User.findAll();
+});
+
+Models.Portfolio.bulkCreate(fakePortfolioData, { individualHooks: true })
+.then(() => {
+  return Models.User.findAll();
+});
+
+Models.UserPortfolio.bulkCreate(fakeUserPortfolioData, { individualHooks: true })
 .then(() => {
   return Models.User.findAll();
 });
