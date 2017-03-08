c29be32012dc50c7b7b6a941e8f1e030a9c2fdd4
Adam Watt <awattny@me.com>
2017-03-07 19:08:15 -0800

    add SPY data to spies table in loadfakeData.js

--- db/loadFakeData.js
+++ db/loadFakeData.js
@@ -5,32 +5,22 @@ const fakeUserData = require('./fakeData/fakeUserData.json');
 const fakeTransactions = require('./fakeData/fakeTransactions.json');
 const fakeUserTransactionData = require('./fakeData/fakeUserTransactionData.json');
 const fakeUserStocksData = require('./fakeData/fakeUserStocksData.json');
-
-console.log(fakeUserData);
+const SPY = require('./fakeData/spyData.json');
 
 Models.User.bulkCreate(fakeUserData, { individualHooks: true })
-.then(() => {
-  return Models.User.findAll();
-});
+.then(() => Models.User.findAll());
 
 Models.Stock.bulkCreate(fakeStockData, { individualHooks: true })
-.then(() => {
-  return Models.User.findAll();
-});
-
+.then(() => Models.User.findAll());
 
 Models.Transaction.bulkCreate(fakeTransactions, { individualHooks: true })
-.then(() => {
-  return Models.User.findAll();
-});
-
+.then(() => Models.User.findAll());
 
 Models.UserTransaction.bulkCreate(fakeUserTransactionData, { individualHooks: true })
-.then(() => {
-  return Models.User.findAll();
-});
+.then(() => Models.User.findAll());
 
 Models.UserStock.bulkCreate(fakeUserStocksData, { individualHooks: true })
-.then(() => {
-  return Models.User.findAll();
-});
+.then(() => Models.User.findAll());
+
+Models.Spy.bulkCreate(SPY, { individualHooks: true })
+.then(() => Models.User.findAll());
