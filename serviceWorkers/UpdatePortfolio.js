dadc1b68306956874f9645aea40846a119ecb6da
Michael <comesm@gmail.com>
2017-03-08 11:36:28 -0800

    cleanup code utilize cron for node

--- serviceWorkers/UpdatePortfolio.js
+++ serviceWorkers/UpdatePortfolio.js
@@ -1,88 +1,81 @@
 const mysql = require('mysql');
 const Promise = require('bluebird');
 const axios = require('axios');
+const CronJob = require('cron').CronJob;
+const fs = require('fs');
 
 Promise.promisifyAll(mysql);
 Promise.promisifyAll(require('mysql/lib/Connection').prototype);
 
 Date.prototype.toMysqlFormat = function () {
-    return this.getUTCFullYear() + "-" +
-    twoDigits(1 + this.getUTCMonth()) + "-" +
-    twoDigits(this.getUTCDate()) + " " +
-    twoDigits(this.getUTCHours()) + ":" +
-    twoDigits(this.getUTCMinutes()) + ":" +
+    return this.getUTCFullYear() + '-' +
+    twoDigits(1 + this.getUTCMonth()) + '-' +
+    twoDigits(this.getUTCDate()) + ' ' +
+    twoDigits(this.getUTCHours()) + ':' +
+    twoDigits(this.getUTCMinutes()) + ':' +
     twoDigits(this.getUTCSeconds());
 };
 function twoDigits(d) {
-    if (0 <= d && d < 10) return "0" + d.toString();
-    if (-10 < d && d < 0) return "-0" + (-1*d).toString();
+    if (0 <= d && d < 10) return '0' + d.toString();
+    if (-10 < d && d < 0) return '-0' + (-1 * d).toString();
     return d.toString();
 }
 
+const job = new CronJob('* * 14 * * 1-5', () => {
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database:'cywdb'
+  database: 'cywdb'
 });
 
 connection.connect();
 
-connection.queryAsync('select * from Users').then(data => {
-  let ids = data.map(val => val.id);
+connection.queryAsync('select * from Users').then(userData => {
+  const ids = userData.map(val => val.id);
 
-  console.log('24', ids);
-var getId = function(inputId, idCounter) {
-  console.log('36', inputId);
+const getId = function (inputId, idCounter) {
+  let idCounterVar = idCounter;
   connection.queryAsync('SELECT stockSymbol, numOfShares, id FROM Stocks INNER JOIN Userstocks ON Stocks.id = Userstocks.StockId WHERE Userstocks.UserId = ?', inputId).then(data => {
-    console.log('28', data);
   if (data.length !== 0) {
-    //console.log('28', data.length);
     let sum = 0;
 
-    var recurse = (stock, counter) => {
-        //let sum = 0;
+    const recurse = (stock, counter) => {
+        let counterVar = counter;
       getPrice(stock.stockSymbol, stock.numOfShares)
         .then(total => {
             sum += total;
-            counter +=1;
-            if(counter < data.length) {
-               recurse(data[counter], counter)
-            }
-            else {
-                console.log('53', sum);
-                idCounter++;
+            counterVar += 1;
+            if (counterVar < data.length) {
+               recurse(data[counterVar], counterVar);
+            } else {
+                idCounterVar++;
                 connection.queryAsync('select id, portfolioValue, cash from (select id, portfolioValue, cash from portfolios inner join userportfolios on userportfolios.PortfolioId = Portfolios.id where userportfolios.UserId = ?) as T', inputId)
-                  .then(portfolioData=> {
-                    console.log('46', portfolioData);
-                    //user's most recent portfolio;
-
-                    let cash = portfolioData[portfolioData.length - 1].cash;
-                    let portId = portfolioData[portfolioData.length - 1].id;
-                    let now = new Date().toMysqlFormat();
+                  .then(portfolioData => {
+                    const cash = portfolioData[portfolioData.length - 1].cash;
+                    const portId = portfolioData[portfolioData.length - 1].id;
                     connection.queryAsync('UPDATE portfolios SET portfolioValue = ?, cash = ? where id= ?', [sum, cash, portId]).then(updatedPortfolio => {
                       console.log('66', updatedPortfolio);
-                      let insertId =  updatedPortfolio.insertId;
+//                      const insertId =  updatedPortfolio.insertId;
                       //console.log('63', updatedPortfolio);
-                    let now = new Date().toMysqlFormat();
+                    const now = new Date().toMysqlFormat();
                     connection.queryAsync('INSERT into portfolios \
                     (date, portfolioValue, cash, createdAt, updatedAt) select date, portfolioValue, cash, createdAt, updatedAt  from portfolios where id = ?', portId)
                     .then(update => {
-                       let insertId = update.insertId;
+                       const insertId = update.insertId;
                        connection.queryAsync('UPDATE portfolios SET date = ? where \
                         id= ?', [now, insertId])
                        .then(() => {
-
-                       connection.queryAsync('INSERT INTO userportfolios \
-                       SET UserId = ?, PortfolioId = ?, createdAt =?, updatedAt=? ', [inputId, insertId, now, now]).then(stuff => {console.log('76', stuff)});
+                       connection.queryAsync('INSERT INTO userportfolios SET UserId = ?, PortfolioId = ?, createdAt \
+                     =?, updatedAt=? ', [inputId, insertId, now, now]);
                     });
                      });
                   });
                 });
                //idCounter++;
-               if (idCounter < ids.length) {
+               if (idCounterVar < ids.length) {
                  //console.log('82', idCounter);
-                 getId(ids[idCounter], idCounter);
+                 getId(ids[idCounterVar], idCounterVar);
               }
             }
           });            //now we have our total MV for that stock
@@ -92,28 +85,20 @@ var getId = function(inputId, idCounter) {
  });
 };
   getId(ids[0], 0);
-
 });
+}, null, true, 'America/Los_Angeles');
 
-var getPrice = ((symbol, shares) => {
+job.start();
 
-
-  return axios.get(`http://dev.markitondemand.com/Api/v2/Quote/json?symbol=${symbol}`).then((stock, err) =>
-      {
-        let price = stock.data.LastPrice;
-        let total = price * shares;
-        console.log('83', total, ' ', symbol);
+const getPrice = ((symbol, shares) =>
+   axios.get(`http://dev.markitondemand.com/Api/v2/Quote/json?symbol=${symbol}`).then((stock) => {
+        const price = stock.data.LastPrice;
+        const total = price * shares;
         return total;
-      }).catch(err => {
-               console.log(err);
-        return 1;
-      })
-});
-
-
-
-
-
-
-
-
+      }).catch(err =>
+        fs.open('./errorLog.txt', 'r+', (error, id) =>
+          fs.write(`${id} ${err}\r\n'`, 'utf8', () =>
+            fs.close(() => console.log('error logged'))
+          ))
+      )
+);
