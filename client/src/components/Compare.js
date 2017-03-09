fc67013d136d6884ced4e8fb329250c827580cd3
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-08 21:29:58 -0800

    create rankings page

--- client/src/components/Compare.js
+++ client/src/components/Compare.js
@@ -2,20 +2,47 @@ import React, { Component } from 'react';
 import { Button, Background, CardSection, Input } from './common';
 import axios from 'axios';
 import { Actions } from 'react-native-router-flux';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 
 class Compare extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			ranking: []
+		};
+	}
+
+	componentDidMount() {
+		// var sortedRanking = ''
+		const context = this;
+		const rankingArray = [];
+		axios.post('http://127.0.0.1:3000/api/portfolio/iyoon@gmail.com')
+		.then(result => {
+				const sortedResult = result.data.sort((x, y) => (y.portfolioValue - x.portfolioValue));
+				sortedResult.map(obj => {
+					if (rankingArray.indexOf(obj.username) === -1) {
+						rankingArray.push(obj.username);
+					}
+				});
+				context.setState({ ranking: rankingArray });
+		});
+	}
 
 	render() {
+		const rankingArray = this.state.ranking.map((result, key) =>
+
+			<View style={styles.columnStyle}>
+				<Text style={styles.textStyle}> {key} </Text>
+				<Text style={styles.textStyle}> {result}</Text>
+			</View>
+		);
+		console.log('ranking', this.state.ranking);
 		return (
 			<Background>
-				<View style={styles.viewStyle}>
-					<View style={styles.columnStyle}>
-						<Text style={styles.textStyle}> YOU </Text>
-						<Text style={styles.textStyle}> VS </Text>
-						<Text style={styles.textStyle}> S&P </Text>
-					</View>
-				</View>
+				<ScrollView>
+					{ rankingArray }
+				</ScrollView>
 			</Background>
 		);
 	}
@@ -23,12 +50,9 @@ class Compare extends Component {
 
 const styles = {
 	viewStyle: {
-		marginTop: 5,
-		marginBottom: 5,
 		marginLeft: 20,
 		marginRight: 20,
 		backgroundColor: 'transparent',
-		justifyContent: 'space-between',
 		flexDirection: 'row',
 		position: 'relative',
 		alignItems: 'center',
