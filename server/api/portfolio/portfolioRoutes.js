597f5ac610055164f4b6b76285b0b1334fd1feb6
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-08 21:30:26 -0800

    create portfolio routes to fetch post data

--- server/api/portfolio/portfolioRoutes.js
+++ server/api/portfolio/portfolioRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const portfolio = require('./portfolioController');
 
 router.route('/:email').get(portfolio.get);
+router.route('/:email').post(portfolio.post);
 
 module.exports = router;
